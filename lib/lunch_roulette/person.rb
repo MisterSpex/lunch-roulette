0c2a5c4decc3f88af943222dc61feaeb5843c537
André Neubauer <andre.neubauer@googlemail.com>
2017-02-09 10:19:26 +0100

    Remove unneeded fields

--- lib/lunch_roulette/person.rb
+++ lib/lunch_roulette/person.rb
@@ -1,30 +1,15 @@
 class LunchRoulette
   class Person
-    attr_accessor :name, :lunchable, :previous_lunches, :features, :team, :specialty, :user_id, :start_date, :table, :email
+    attr_accessor :name, :lunchable, :previous_lunches, :team, :user_id
     def initialize(hash)
       @lunchable = %w(true TRUE).include? hash['lunchable']
       @team = hash['team']
       @user_id = hash['user_id']
-      @email = hash['email']
       @name = hash['name']
       @previous_lunches = []
       if hash['previous_lunches']
         @previous_lunches = hash['previous_lunches'].split(',').map{|i| i.to_i }
-        config.maxes['lunch_id'] = @previous_lunches.max if @previous_lunches && (@previous_lunches.max > config.maxes['lunch_id'].to_i)
-        # Generate previous lunches to person mappings:
-        
       end
-    end
-
-    def inspect
-      s = @name
-      if @specialty
-        s += " (#{@team} - #{@specialty}"
-      else
-        s += " (#{@team}"
-      end
-      s += ", Table #{@table})"
-      s
     end
 
     def config
